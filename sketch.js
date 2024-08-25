c80fbb15a503904cbee7ec7f09ffa0187f10fed7
Evelyn <00001141342534SP@aluno.educacao.sp.gov.br>
2024-08-25 18:19:51 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+funtion setup() {
+    createCanvas(600, 600);
+    background("black)";
+  }
+  
+  funtion draw() {
+    stroke ("blue");
+    fill ("red");
+    
+  if (mouseisPressed){
+      rect(mouseX,mouseY, 20, 35);
+    }
+    
+  }
+  
