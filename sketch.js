cf95a99bfeda51f83c9b09218a057265ab8aaa3b
Evelyn <00001141342534SP@aluno.educacao.sp.gov.br>
2024-08-25 18:30:28 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
-funtion setup() {
-    createCanvas(600, 600);
-    background("black)";
+function setup() {
+  createCanvas(600, 600);
+  background("black");
+}
+
+function draw() {
+  stroke ("blue");
+  fill ("red");
+  
+ if(mouseIsPressed){
+    rect(mouseX,mouseY, 20, 35);
   }
   
-  funtion draw() {
-    stroke ("blue");
-    fill ("red");
-    
-  if (mouseisPressed){
-      rect(mouseX,mouseY, 20, 35);
-    }
-    
-  }
+}
   
